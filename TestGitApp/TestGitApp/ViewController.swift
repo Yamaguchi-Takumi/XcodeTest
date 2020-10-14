7a63eed62317d932c1a4bfe94777746e4ce39662
Yamaguchi-Takumi <t_yamaguchi@msk2.co.jp>
2020-10-14 09:36:52 +0900

    initial commit
    練習です。

---
+++ TestGitApp/TestGitApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TestGitApp
+//
+//  Created by 山口拓海 on 2020/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("TestGitApp")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
